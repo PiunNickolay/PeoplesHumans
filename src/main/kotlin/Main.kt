97723e8c9b03c33b944d2b922a154c0cb724b41b
PiunNickolay <147736400+PiunNickolay@users.noreply.github.com>
2024-06-03 23:05:15 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 package ru.netology
 
 fun main() {
-    val likes = 21
-    if(likes%10==1 && likes!=11){
+    val likes = 511
+    if(likes%10==1 && likes!=11 && likes%100 != 11){
         println("Понравилось $likes человеку")
     }else{
         println("Понравилось $likes людям")
-    }
 }
